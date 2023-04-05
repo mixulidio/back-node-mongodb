edf9801537584e877e5cfe536ed7f89069f7d1f1
Paulo Vinicius <mixulidio@gmail.com>
2023-04-05 07:35:00 -0300

    connect

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,7 +1,5 @@
 import mongoose from "mongoose";
 // 635178034724c1716938644d
-//mongoose.connect("mongodb+srv://mixulidio:MongoPaulo2023@cluster0.kpnuiig.mongodb.net/alura-node");
-mongoose.connect("mongodb+srv://mixulidio:MongoPaulo2023@cluster0.kpnuiig.mongodb.net/alura-node");
 
 let db = mongoose.connection;
 export default db;
